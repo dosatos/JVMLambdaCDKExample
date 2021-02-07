9cdab65b1f4c6038e17c6d3a947621b44923d534
Yeldos Balgabekov <byeldos@amazon.com>
2021-02-07 20:02:43 +0100

    init commit

--- lib/jvm_lambda-stack.ts
+++ lib/jvm_lambda-stack.ts
@@ -1,9 +1,17 @@
+import * as path from 'path';
+
+import * as lambda from '@aws-cdk/aws-lambda';
 import * as cdk from '@aws-cdk/core';
 
 export class JvmLambdaStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const fn = new lambda.Function(this, 'MyFunction', {
+      runtime: lambda.Runtime.NODEJS_10_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset(path.join(__dirname, 'lambda-handler')),
+    });
+
   }
 }
